b5f3072d774a5be960ff4da8b8829fb2fa8b7235
Yana <zubalana0@gmail.com>
2026-03-31 06:34:24 +0300

    feat(backend): set up authomatic logging in right after registration

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,12 +2,19 @@ import express from 'express';
 import User from '../models/User';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import { userInfo } from 'node:os';
 
 const router = express.Router();
 
 router.post('/register', async (req, res)=>{
     try{
       const { name, email, password } = req.body;
+
+      const existingUser = await User.findOne({email});
+      
+      if(existingUser){
+        return res.status(400).json({message: 'User already exists'});
+      }
       const hashedPassword = await bcrypt.hash(password, 10);
 
       const newUser = new User({
@@ -17,7 +24,22 @@ router.post('/register', async (req, res)=>{
       })
 
       await newUser.save();
-      res.status(201).json({message: 'User registered successfully'});
+
+      const token = jwt.sign(
+        {id: newUser._id},
+        process.env.JWT_SECRET,
+        {expiresIn: '24h'}
+      )
+
+      res.status(200).json({
+        message: 'User registered successfully',
+        token,
+        user: {
+            id: newUser._id,
+            name: newUser.name,
+            email: newUser.email
+        }
+      })
     }catch(err){
         console.error('Error registering user:', err);
         res.status(500).json({message: err.message});
@@ -33,7 +55,7 @@ router.post('/login', async (req, res)=>{
         return res.status(400).json({message: "User not found"});
     }
 
-    const passwordMatch = bcrypt.compare(password, user.password);
+    const passwordMatch = await bcrypt.compare(password, user.password);
 
     if(!passwordMatch){
         return res.status(400).json({message: "Invalid credentials"});
