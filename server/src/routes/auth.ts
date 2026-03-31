1c52a19b740f77aa27a866d7ba8b1233bb14de16
Yana <zubalana0@gmail.com>
2026-03-31 06:19:25 +0300

    feat(backend): create login route, sign a jwt token

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -24,4 +24,38 @@ router.post('/register', async (req, res)=>{
     }
 })
 
+router.post('/login', async (req, res)=>{
+    try{
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+
+    if(!user){
+        return res.status(400).json({message: "User not found"});
+    }
+
+    const passwordMatch = bcrypt.compare(password, user.password);
+
+    if(!passwordMatch){
+        return res.status(400).json({message: "Invalid credentials"});
+    }
+
+    const token = jwt.sign(
+        {id: user._id},
+        process.env.JWT_SECRET,
+        {expiresIn: '24h'}
+    )
+
+    res.status(200).json({
+        token,
+        user: {
+            id: user._id,
+            name: user.name,
+            email: user.email
+        }
+    });
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
+})
+
 export default router;
