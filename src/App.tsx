7256c66232315ad9b79dfc2f54e5345e1c1a062c
Yana <zubalana0@gmail.com>
2026-03-31 05:10:32 +0300

    feat(frontend): set up a glow for the progress bar, download and set up the fonts, change default text color

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
-
+import Week from './components/Week'
 function App() {
 
   return (
-    <>
-    </>
+    <div className='w-full h-full'>
+      <Week/>
+    </div>
   )
 }
 
---
+++ src/components/Week.tsx
@@ -0,0 +1,8 @@
+import WeekDay from "./WeekDay"
+export default function Week(){
+    return(
+      <div className="w-full flex justify-between items-center">
+        <WeekDay/>
+      </div>
+    )
+}
