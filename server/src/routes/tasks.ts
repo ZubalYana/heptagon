7632142f233f6368b65d04ac1dbf3bb6db480a78
Yana <zubalana0@gmail.com>
2026-04-08 05:56:39 +0300

    feat(frontend): actually wire the task creation popup to backend, deprive of minor bugs

--- server/src/routes/tasks.ts
+++ server/src/routes/tasks.ts
@@ -8,10 +8,10 @@ router.use(authMiddleware);
 router.post('/', async (req,res)=>{
     try{
         const { text, priority } = req.body;
-        const task = await Task.create(text, priority);
+        const task = await Task.create({text, priority});
         res.status(202).json(task);
     }catch(err){
-        return res.status(500).json({message: 'Failed to create task.'});
+        return res.status(500).json({message: 'Failed to create task:', err});
     }
 })
 
--- src/components/popups/TaskCreation.tsx
+++ src/components/popups/TaskCreation.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Input from "../customElements/Input";
 import Button from "../customElements/PrimaryButton";
 import Select from "../customElements/Select";
+import Alert from "../customElements/Alert";
+import { AnimatePresence } from "framer-motion";
 
 interface TaskCreationProps {
   day: string;
@@ -10,30 +12,89 @@ interface TaskCreationProps {
 export default function TaskCreation({ day }: TaskCreationProps) {
   const [text, setText] = useState<string>("");
   const [priority, setPriority] = useState<string>("");
+  const [alert, setAlert] = useState<{
+    shown: boolean;
+    type: "success" | "info" | "error";
+    text: string;
+  }>({ shown: false, type: "info", text: "" });
+
+  function createTask() {
+    try {
+      const token = localStorage.getItem("token");
+      if (!text && !priority) {
+        setAlert({
+          shown: true,
+          type: "info",
+          text: "Text and priority are required.",
+        });
+        return;
+      }
+      fetch("http://localhost:5000/tasks/", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer: ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text, priority }),
+      }).then((res) => {
+        console.log(res);
+        if (!res.ok) {
+          setAlert({
+            shown: true,
+            type: "error",
+            text: "Server failed to create task.",
+          });
+          return;
+        }
+        setAlert({
+          shown: true,
+          type: "success",
+          text: "Task created successfully!",
+        });
+      });
+    } catch (err) {
+      console.error("Error creating task:", err);
+      setAlert({ shown: true, type: "error", text: "Error creating task" });
+    }
+  }
+
+  function closeAlert() {
+    setAlert({ shown: false, text: "", type: "info" });
+  }
 
   return (
-      <div 
+    <div
       className="lg:w-[40%] bg-[#1F1F1F] rounded-md p-4 flex flex-col items-center"
-      onClick={(e)=>e.stopPropagation()}
-      >
-        <h3 className="text-[20px] font-medium mb-4">Create a task for {day}</h3>
-        <Input 
-        placeholder="Task text" 
-        onChange={(e) => setText(e.target.value)} 
+      onClick={(e) => e.stopPropagation()}
+    >
+      <h3 className="text-[20px] font-medium mb-4">Create a task for {day}</h3>
+      <Input
+        placeholder="Task text"
+        onChange={(e) => setText(e.target.value)}
         value={text}
-        />
-        <Select
-          options={[
-            { value: "high", label: "High priority" },
-            { value: "medium", label: "Medium priority" },
-            { value: "optional", label: "Optional priority" },
-          ]}
-          value={priority}
-          onChange={(value) => setPriority(value)}
-          className="mt-2"
-        />
-        <Button children="Create task" className="mt-4" />
-      </div>
-    
+      />
+      <Select
+        options={[
+          { value: "high", label: "High" },
+          { value: "medium", label: "Medium" },
+          { value: "optional", label: "Optional" },
+        ]}
+        value={priority}
+        placeholder="Select task priority"
+        onChange={(value) => setPriority(value)}
+        className="mt-2"
+      />
+      <Button
+        onClick={() => createTask()}
+        children="Create task"
+        className="mt-4"
+      />
+
+      <AnimatePresence>
+        {alert.shown && (
+          <Alert type={alert.type} text={alert.text} onClose={closeAlert} />
+        )}
+      </AnimatePresence>
+    </div>
   );
 }
