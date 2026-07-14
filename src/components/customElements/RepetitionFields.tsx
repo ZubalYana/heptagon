641214845eb753191e4dae5308fabb6fdb2f8666
Yana <zubalana0@gmail.com>
2026-07-14 13:52:18 +0300

    feat(frontend): change the days display order: from Monday to Sunday

--- src/components/customElements/RepetitionFields.tsx
+++ src/components/customElements/RepetitionFields.tsx
@@ -4,13 +4,13 @@ import Select from "./Select";
 import type { Repetition } from "../../interfaces/Task";
 
 const WEEKDAYS = [
-  { label: "S", value: 0 },
   { label: "M", value: 1 },
   { label: "T", value: 2 },
   { label: "W", value: 3 },
   { label: "T", value: 4 },
   { label: "F", value: 5 },
   { label: "S", value: 6 },
+  { label: "S", value: 0 },
 ];
 
 const DEFAULT_REPETITION: Repetition = {
