4d0ce2203f426835aafb7c235e592b7b163cdb68
Yana <zubalana0@gmail.com>
2026-08-20 07:57:50 +0300

    refactor: finish repository optimization, insure ownership checking

--- server/src/features/tasks/taskRepository.ts
+++ server/src/features/tasks/taskRepository.ts
@@ -18,13 +18,12 @@ export const taskRepository = {
   },
 
   async getByDay(userId: string, dayId: string) {
-    const day = await Day.findOne({ _id: dayId, userId: userId }).populate(
-      "tasks"
-    );
+    const day = await Day.findOne({ _id: dayId, userId }).populate("tasks");
     if (!day) throw new Error("Day not found in database records.");
 
     const allRegularTasks = await Task.find({
-      repetition: { $ne: null, userId: userId },
+      userId,
+      repetition: { $ne: null },
     });
     const occurringTasks = allRegularTasks.filter((task) =>
       occursOn(task, day.date)
@@ -33,16 +32,16 @@ export const taskRepository = {
     return [...day.tasks, ...occurringTasks];
   },
 
-  async toggle(taskId: string, dayId: string) {
-    const task = await Task.findById(taskId);
+  async toggle(userId: string, taskId: string, dayId: string) {
+    const task = await Task.findOne({ _id: taskId, userId });
     if (!task) throw new Error("Task not found in database records.");
 
     let updated: TaskType | null;
 
     if (task.repetition === null) {
       const newCompletedStatus = !task.completed;
-      updated = await Task.findByIdAndUpdate(
-        taskId,
+      updated = await Task.findOneAndUpdate(
+        { _id: taskId, userId },
         {
           $set: {
             completed: newCompletedStatus,
@@ -71,107 +70,144 @@ export const taskRepository = {
             },
           };
 
-      updated = await Task.findByIdAndUpdate(taskId, updateOperation, {
-        returnDocument: "after",
-      });
+      updated = await Task.findOneAndUpdate(
+        { _id: taskId, userId },
+        updateOperation,
+        { returnDocument: "after" }
+      );
     }
 
     if (!updated) throw new Error("Task not found after update.");
     return updated;
   },
 
-  async edit(taskId: string, text: string, priority: Priority, repetition: Repetition){
-    const updated = await Task.findByIdAndUpdate(
-        {_id: taskId},
-        { $set: {text, priority, repetition}},
-        {returnDocument: "after"}
-    )
-    if(!updated) throw new Error('Task not found in database records.')
+  async edit(
+    userId: string,
+    taskId: string,
+    text: string,
+    priority: Priority,
+    repetition: Repetition
+  ) {
+    const updated = await Task.findOneAndUpdate(
+      { _id: taskId, userId },
+      { $set: { text, priority, repetition } },
+      { returnDocument: "after" }
+    );
+    if (!updated) throw new Error("Task not found in database records.");
     return updated;
   },
 
-  async delete(taskId: string){
-    const task = await Task.findByIdAndDelete(taskId);
-    if(!task) throw new Error('Task not found')
-    return "Deleted successfully"
+  async delete(userId: string, taskId: string) {
+    const task = await Task.findOneAndDelete({ _id: taskId, userId });
+    if (!task) throw new Error("Task not found");
+    return "Deleted successfully";
   },
 
-  async addSubtask(taskId: string, taskDate: string, subtaskText: string){
+  async addSubtask(
+    userId: string,
+    taskId: string,
+    taskDate: string,
+    subtaskText: string
+  ) {
     let newDateArray: string[] = [];
-    const parentalTask = await Task.findById(taskId);
-    if(!parentalTask) throw new Error("Parental task not found")
-    parentalTask.subtasks.push({subtaskText})
+    const parentalTask = await Task.findOne({ _id: taskId, userId });
+    if (!parentalTask) throw new Error("Parental task not found");
+    parentalTask.subtasks.push({ text: subtaskText });
 
-    if(parentalTask.repetition){
-        newDateArray = parentalTask.completedDates.includes(taskDate)?
-        parentalTask.completedDates.filter((d)=>d!=taskDate) : parentalTask.completedDates
+    if (parentalTask.repetition) {
+      newDateArray = parentalTask.completedDates.includes(taskDate)
+        ? parentalTask.completedDates.filter((d) => d != taskDate)
+        : parentalTask.completedDates;
 
-        parentalTask.completedDates = newDateArray;
-    }else{
-        parentalTask.completed = parentalTask.completed? false: parentalTask.completed;
+      parentalTask.completedDates = newDateArray;
+    } else {
+      parentalTask.completed = parentalTask.completed
+        ? false
+        : parentalTask.completed;
     }
 
-    await parentalTask.save()
-    return parentalTask
-  },
-
-  async toggleSubtask(taskId: string, subtaskId: string, date: string){
-    const parentalTask = await Task.findById(taskId);
-    if(!parentalTask) throw new Error("Parental task not found in database records.")
-
-    const subtask = parentalTask.subtasks.id(subtaskId);
-    if(!subtask) throw new Error("Subtask not found in database records.")
-    
-    if(parentalTask.repetition === null){
-        subtask.completed = !subtask.completed
-
-        const uncompletedSubtasks = parentalTask.subtasks.filter((s)=>s.completed == false);
-        if(uncompletedSubtasks.length === 0){
-            parentalTask.completed = true;
-        }else{
-            parentalTask.completed = false;
-        }
-    }else{
-        subtask.completedDates.includes(date)?
-        subtask.completedDates = subtask.completedDates.filter((d) => d !== date) :
-        subtask.completedDates.push(date)
-
-        const uncompletedSubtasks = parentalTask.subtasks.filter((s)=>!s.completedDates.includes(date))
-        if(uncompletedSubtasks.length === 0){
-            parentalTask.completedDates.includes(date)?
-            parentalTask.completedDates = parentalTask.completedDates :
-            parentalTask.completedDates.push(date)
-        }else{
-            parentalTask.completedDates.includes(date)?
-            parentalTask.completedDates = parentalTask.completedDates.filter((d)=>d!=date) :
-            parentalTask.completedDates = parentalTask.completedDates
-        }
-    }
-
-    await parentalTask.save()
+    await parentalTask.save();
     return parentalTask;
   },
 
-  async editSubtask(taskId: string, subtaskId: string, text: string){
-    const parentalTask = await Task.findOne({_id: taskId});
-    if(!parentalTask) throw new Error("Parental task not found in database records.")
+  async toggleSubtask(
+    userId: string,
+    taskId: string,
+    subtaskId: string,
+    date: string
+  ) {
+    const parentalTask = await Task.findOne({ _id: taskId, userId });
+    if (!parentalTask)
+      throw new Error("Parental task not found in database records.");
 
     const subtask = parentalTask.subtasks.id(subtaskId);
-    if(!subtask) throw new Error("Subtask not found in database records.")
-    
+    if (!subtask) throw new Error("Subtask not found in database records.");
+
+    if (parentalTask.repetition === null) {
+      subtask.completed = !subtask.completed;
+
+      const uncompletedSubtasks = parentalTask.subtasks.filter(
+        (s) => s.completed == false
+      );
+      if (uncompletedSubtasks.length === 0) {
+        parentalTask.completed = true;
+      } else {
+        parentalTask.completed = false;
+      }
+    } else {
+      subtask.completedDates.includes(date)
+        ? (subtask.completedDates = subtask.completedDates.filter(
+            (d) => d !== date
+          ))
+        : subtask.completedDates.push(date);
+
+      const uncompletedSubtasks = parentalTask.subtasks.filter(
+        (s) => !s.completedDates.includes(date)
+      );
+      if (uncompletedSubtasks.length === 0) {
+        parentalTask.completedDates.includes(date)
+          ? (parentalTask.completedDates = parentalTask.completedDates)
+          : parentalTask.completedDates.push(date);
+      } else {
+        parentalTask.completedDates.includes(date)
+          ? (parentalTask.completedDates = parentalTask.completedDates.filter(
+              (d) => d != date
+            ))
+          : (parentalTask.completedDates = parentalTask.completedDates);
+      }
+    }
+
+    await parentalTask.save();
+    return parentalTask;
+  },
+
+  async editSubtask(
+    userId: string,
+    taskId: string,
+    subtaskId: string,
+    text: string
+  ) {
+    const parentalTask = await Task.findOne({ _id: taskId, userId });
+    if (!parentalTask)
+      throw new Error("Parental task not found in database records.");
+
+    const subtask = parentalTask.subtasks.id(subtaskId);
+    if (!subtask) throw new Error("Subtask not found in database records.");
+
     subtask.text = text;
-    await subtask.save()
+    await parentalTask.save();
     return subtask;
   },
 
-  async deleteSubtask(taskId: string, subtaskId: string){
-    const parentalTask = await Task.findById({_id: taskId});
-    if(!parentalTask) throw new Error("Parental task not found in database records.")
-    
-    parentalTask.subtasks.id(subtaskId).deleteOne()
+  async deleteSubtask(userId: string, taskId: string, subtaskId: string) {
+    const parentalTask = await Task.findOne({ _id: taskId, userId });
+    if (!parentalTask)
+      throw new Error("Parental task not found in database records.");
 
-    await parentalTask.save()
+    parentalTask.subtasks.id(subtaskId)!.deleteOne();
 
-    return "Deleted successfully"
-  }
+    await parentalTask.save();
+
+    return "Deleted successfully";
+  },
 };
